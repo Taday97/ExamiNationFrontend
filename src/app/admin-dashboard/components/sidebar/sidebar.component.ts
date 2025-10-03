e7dc9b6ccef598a3ecd7a20cfa526700619a8932
tadayglezchav <tadayglezchav@gmail.com>
2025-10-03 13:30:57 +0200

    sidebar

--- src/app/admin-dashboard/components/sidebar/sidebar.component.ts
+++ src/app/admin-dashboard/components/sidebar/sidebar.component.ts
@@ -17,16 +17,16 @@ export class SidebarComponent {
 
 
   menuItems = [
-    {
-      route: '/admin/dashboard',
-      label: 'Dashboard',
-      iconSvg: this.sanitizer.bypassSecurityTrustHtml(`
-          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
-            <path fill="currentColor"
-              d="M19 5v2h-4V5zM9 5v6H5V5zm10 8v6h-4v-6zM9 17v2H5v-2zM21 3h-8v6h8zM11 3H3v10h8zm10 8h-8v10h8zm-10 4H3v6h8z" />
-          </svg>
-      `),
-    },
+    // {
+    //   route: '/admin/dashboard',
+    //   label: 'Dashboard',
+    //   iconSvg: this.sanitizer.bypassSecurityTrustHtml(`
+    //       <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+    //         <path fill="currentColor"
+    //           d="M19 5v2h-4V5zM9 5v6H5V5zm10 8v6h-4v-6zM9 17v2H5v-2zM21 3h-8v6h8zM11 3H3v10h8zm10 8h-8v10h8zm-10 4H3v6h8z" />
+    //       </svg>
+    //   `),
+    // },
     {
       route: '/admin/tests',
       label: 'Tests',
@@ -105,20 +105,20 @@ export class SidebarComponent {
           </svg>
          `),
         },
-        {
-          route: '/admin/roles',
-          label: 'Roles',
-          iconSvg: this.sanitizer.bypassSecurityTrustHtml(`
-          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
-            <g fill="none" stroke="currentColor" stroke-width="1.8">
-              <path
-                d="M3 10.417c0-3.198 0-4.797.378-5.335c.377-.537 1.88-1.052 4.887-2.081l.573-.196C10.405 2.268 11.188 2 12 2s1.595.268 3.162.805l.573.196c3.007 1.029 4.51 1.544 4.887 2.081C21 5.62 21 7.22 21 10.417v1.574c0 5.638-4.239 8.375-6.899 9.536C13.38 21.842 13.02 22 12 22s-1.38-.158-2.101-.473C7.239 20.365 3 17.63 3 11.991z" />
-              <circle cx="12" cy="9" r="2" />
-              <path d="M16 15c0 1.105 0 2-4 2s-4-.895-4-2s1.79-2 4-2s4 .895 4 2Z" />
-            </g>
-          </svg>
-         `),
-        },
+        // {
+        //   route: '/admin/roles',
+        //   label: 'Roles',
+        //   iconSvg: this.sanitizer.bypassSecurityTrustHtml(`
+        //   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+        //     <g fill="none" stroke="currentColor" stroke-width="1.8">
+        //       <path
+        //         d="M3 10.417c0-3.198 0-4.797.378-5.335c.377-.537 1.88-1.052 4.887-2.081l.573-.196C10.405 2.268 11.188 2 12 2s1.595.268 3.162.805l.573.196c3.007 1.029 4.51 1.544 4.887 2.081C21 5.62 21 7.22 21 10.417v1.574c0 5.638-4.239 8.375-6.899 9.536C13.38 21.842 13.02 22 12 22s-1.38-.158-2.101-.473C7.239 20.365 3 17.63 3 11.991z" />
+        //       <circle cx="12" cy="9" r="2" />
+        //       <path d="M16 15c0 1.105 0 2-4 2s-4-.895-4-2s1.79-2 4-2s4 .895 4 2Z" />
+        //     </g>
+        //   </svg>
+        //  `),
+        // },
       ],
     },
   ];
